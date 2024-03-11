c9b2ccbfee2cd821a7e59255c11a9d1dcdecee5f
sara vilela <sara.bizerra@icloud.com>
2024-03-11 15:18:53 -0300

    Adição dos efeitos de hover, estilização da erceira seçãoe termino do projeto

--- scripts.js
+++ scripts.js
@@ -2,18 +2,18 @@ const botaoAbrir = document.querySelector("header > button")
 const botaoFechar = document.querySelector("header nav button")
 
 const body = document.querySelector("body")
-const nav = document.querySelector("header.mobile")
+const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener(`click`, abrirMenu)
 botaoFechar.addEventListener(`click`,fecharMenu)
 
-function abrirMenu(){
+function abrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
     
 }
 
-function fecharMenu(){
+function fecharMenu() {
     body.classList.remove("escurecer")
     nav.classList.remove("abrir")
 }   
